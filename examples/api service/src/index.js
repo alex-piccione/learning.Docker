cf2f61bea2e8a371e6eeace146d56d007341d383
Alex <alessandro.piccione.75@gmail.com>
2022-07-30 09:21:11 +0100

    correct use of MongoDB

--- examples/api service/src/index.js
+++ examples/api service/src/index.js
@@ -26,9 +26,23 @@ server.listen(config.serverPort, () => {
 server.get("/", (req, res) => {
     const body = `<html>
         <h1>Hello World <small>(version: ${version})</small></h1>
-        <p>A: ${secrets && secrets.A}</p>
-    </html>`;
-    //res.send(body, { "Content-Type": "text/plain" }, 200)
+        <p>A: ${secrets && secrets.Test}</p>
+
+        <h3>Pages</h3>
+        <menu>            
+            <ul>
+                <li><a href="/currencies">Currencies</a></li>
+            </ul>
+        </menu>
+
+        <h3>API</h3>
+        <menu>
+            <ul>
+                <li>GET /api/currency : List of all currencies</li>
+            </ul>
+        </menu>
+    </html>`
+
     res.send(body)
 })
 
@@ -40,9 +54,39 @@ server.get("/api/info", (req, res) => {
         )
 })
 
-server.get("/api/currency", async (req, res) => {
+server.get("/currencies", (req, res) => { 
 
-    const currencies = await repository.getCurencies()
-
-    return res.json(currencies)
+    const formatData = (currencies) => `<html>    
+        <h1>Currencies</h1>
+        <div><a href="/">Home</a></div>
+        <hr>
+        <table>                     
+            <tr>            
+                <th>Code</th>
+                <th>Name</th>
+            </tr>
+            ${currencies.map(currency => `<tr>
+                <td>${currency.Code}</td>
+                <td>${currency.Name}</td>
+            </tr>`).join("")}   
+        </table>
+    </html>`
+    
+    repository.getCurencies()
+        .then(data => res.send(formatData(data), { "Content-Type": "text/html" }, 200))
+        .catch(error => createErrorHtml(error), 500)
 })
+
+server.get("/api/currency", (req, res) => {
+    repository.getCurencies()
+        .then(data => res.json(data))
+        .catch(error => res.json(createError(error), 500))
+})
+
+
+const createError = (error) => { return {"Message": `${error}`} }
+
+const createErrorHtml = (error) => `<html>
+    <h1>Error</h1>
+        <p>${error}</p>
+    </html>`
--- examples/api service/src/repository.js
+++ examples/api service/src/repository.js
@@ -1,25 +1,49 @@
-//const { MongoClient, ServerApiVersion } = require('mongodb');
 import {MongoClient, ServerApiVersion} from 'mongodb'
-
 
 const Repository = ((username, password) => {
 
     const uri = `mongodb+srv://${username}:${password}@cluster0.74mtl.mongodb.net/?retryWrites=true&w=majority`
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
 
-    client.connect(err => {
-        const collection = client.db("Portfolio").collection("Currency");
-        // perform actions on the collection object
-        client.close();
-      });
+    // .connet is not required ???
+    /*const connect = () => {
+        client.connect(err => {
+            if (err) {
+                console.log(`MongoDB client connect fails. ${err}`)
+                //throw(`Client connect fails. ${err}`)
+                setTimeout(connect(), 1000)
+            }
+            console.log(`MongoDB client connection OK. ${err}`)
+          });
+    }*/
+
+    //connect()
+
 
     return {
-        getCurencies: async () => {
-            return [
-                {id:"EUR", name: "Euro"},{ id:"ADA", name: "Cardano"},
-            ]
-        }
+        getCurencies: () => new Promise((resolve, reject) => {
+            console.log("getCurencies")
+            try {
+                client.db("Portfolio").collection("Currency")
+                    .find().toArray((err, documents) => {
+                        if (err) {
+                            console.log(`Collection command fails. ${err}`)
+                            reject(`Collection command fails. ${err}`)
+                        }  
+                        const currencies = parseCurrencies(documents)                      
+                        resolve(currencies)
+                    })                
+            }
+            catch (error) {
+                console.log("error")
+                reject(`${error}`)
+            }
+        }),   
     }
 })
+
+const parseCurrencies = documents => documents.map(
+    document => ({Code: document._id, Name: document.Name})
+)
 
 export default Repository
